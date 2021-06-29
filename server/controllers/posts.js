d26d669fd920ace0f4322e9cf1aeda5d7ab67ce9
arthurs14 <arthurcs426@gmail.com>
2021-06-29 12:55:18 -0700

    working on the front end look of the web app

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,29 @@
 /* handlers for our routes */
+import PostMessage from '../models/postMessage.js';
 
-export const getPosts = (req, res) => {
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    
+    console.log(postMessages);
+
+    res.status(200).json(postMessage);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
   res.send('this is working');
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();  
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error });
+  }
+  res.send('Post Creation');
 };
