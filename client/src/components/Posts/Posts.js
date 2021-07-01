9d8adc578586945cc59cddaed4535613c3f1946c
arthurs14 <arthurcs426@gmail.com>
2021-07-01 12:52:16 -0700

    was getting http headers error on the backend - should be fixed now

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -7,6 +7,8 @@ import useStyles from './styles';
 const Posts = () => {
   const posts = useSelector(state => state.posts);
   const classes = useStyles();
+
+  console.log(posts);
   
   return (
     <>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,9 +5,10 @@ export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
     
-    console.log(postMessages);
+    console.log('postMessages:', postMessages);
 
-    res.status(200).json(postMessage);
+    return res.status(200).json(postMessages);
+    
   } catch (error) {
     res.status(404).json({ message: error });
   }
@@ -21,7 +22,7 @@ export const createPost = async (req, res) => {
   try {
     await newPost.save();  
 
-    res.status(201).json(newPost);
+    return res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error });
   }
