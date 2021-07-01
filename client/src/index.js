39cb56f96604a4eb9c4cb62f99a7667ddda4d7ee
arthurs14 <arthurcs426@gmail.com>
2021-07-01 13:23:33 -0700

    added a svg background

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import App from './App';
+import './index.css';
 
 // gets the index file
 import reducers from './reducers';
