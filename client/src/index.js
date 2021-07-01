68a41b35cceef63dc7e858a736a73ae37183b7df
arthurs14 <arthurcs426@gmail.com>
2021-06-30 18:51:01 -0700

    finished up reducers files

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import reducers from './store/reducers';
 import App from './App';
+
+// gets the index
+import reducers from './reducers';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
