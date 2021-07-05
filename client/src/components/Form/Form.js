67b180a7e6da5dd7ed5e7825a7d0cab1bc5fda73
arthurs14 <arthurcs426@gmail.com>
2021-07-05 12:52:51 -0700

    form is repopulated with card info when clicked on edit icon

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import useStyles from './styles';
 import{ createPost, updatePost } from '../../actions/postActions';
@@ -14,9 +15,14 @@ const Form = ({ currentId, setCurrentId }) => {
     tags: '',
     selectedFile: '',
   });
+  const post = useSelector(state => currentId ? state.posts.find(post => post._id === currentId) : null);
   const classes = useStyles();
 
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
 
   // submit form user input data
   const handleSubmit = (e) => {
