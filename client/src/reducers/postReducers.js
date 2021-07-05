95ffefd8ba7e5925553310f24548af3a2f0eb8f2
arthurs14 <arthurcs426@gmail.com>
2021-07-05 12:47:11 -0700

    updated reducer for updating a post

--- client/src/reducers/postReducers.js
+++ client/src/reducers/postReducers.js
@@ -2,6 +2,8 @@ const initialState = [];
 
 const reducer = (posts = initialState, action) => {
   switch(action.type) {
+    case 'UPDATE':
+      return posts.map((post) => post._id === action.payload._id ? action.payload : post);
     case 'FETCH_ALL':
       return action.payload;
     case 'CREATE':
