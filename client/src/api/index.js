46d64493bb427443bb2cbd684255c6bcf04d7cab
arthurs14 <arthurcs426@gmail.com>
2021-07-05 12:44:47 -0700

    setting up actions for redux and etc

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,3 +5,4 @@ const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (post) => axios.post(url, post);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,9 +4,9 @@ import FileBase from 'react-file-base64';
 import { useDispatch } from 'react-redux';
 
 import useStyles from './styles';
-import{ createPost } from '../../actions/postActions';
+import{ createPost, updatePost } from '../../actions/postActions';
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     creator: '',
     title: '',
@@ -22,7 +22,12 @@ const Form = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    dispatch(createPost(postData))
+    if (currentId) {
+      dispatch(updatePost(currentId, postData));
+    } else {
+      dispatch(createPost(postData));
+    }
+
   };
 
   const clear = () => {};
