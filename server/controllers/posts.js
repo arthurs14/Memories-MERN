3b3b841e038b9bbcfa79524887b126ed7f7a856b
arthurs14 <arthurcs426@gmail.com>
2021-07-05 12:33:33 -0700

    took out some console logs and added a new function in controllers for updating a post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,11 +1,10 @@
 /* handlers for our routes */
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
-    
-    console.log('postMessages:', postMessages);
 
     return res.status(200).json(postMessages);
     
@@ -27,4 +26,15 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: error });
   }
   //res.send('Post Creation');
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+  res.json(updatedPost);
 };
