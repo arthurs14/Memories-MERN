5d0ce4bca511aba0f3bbbcc7d0f6e24d8cfa89b4
arthurs14 <arthurcs426@gmail.com>
2021-07-07 18:12:11 -0700

    moved monbo url and port to an env file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import keys from './config/keys.js';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
@@ -14,10 +15,9 @@ app.use(cors());
 // will start all routes with /posts
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = keys.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, 
+mongoose.connect(process.env.CONNECTION_URL, 
   { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
