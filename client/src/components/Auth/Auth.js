fd7854fa32eab28eb5520dc23d55b2a9093c69ff
arthurs14 <arthurcs426@gmail.com>
2021-07-07 18:56:10 -0700

    turning app.js into components to clean it up

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,21 @@
+import { 
+  Avatar, 
+  Button, 
+  Paper, 
+  Grid, 
+  Typography, 
+  Container 
+} from '@material-ui/core';
+import useStyles from './styles';
+
+const Auth = () => {
+  const state = null;
+
+  return (
+    <div>
+      AUTH
+    </div>
+  );
+};
+
+export default Auth;
