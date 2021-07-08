9db998086dafdc1619c9f28fb5288c8462108e86
arthurs14 <arthurcs426@gmail.com>
2021-07-07 17:56:08 -0700

    fixing spacing on likes and fixing orientation of form when on mobile devices

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -45,21 +45,22 @@ const Post = ({ post, setCurrentId }) => {
           gutterBottom>{post.title}</Typography>
       <CardContent>
         <Typography
-          variant="h5" 
-          gutterBottom>{post.message}</Typography>
+          variant="body2"
+          color="textSecondary"
+          component="p">{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button 
           size="small" 
           color="primary" 
           onClick={() => dispatch(likePost(post._id))}>
-          <ThumbUpAltIcon fontSize="small"/> Like {post.likeCount}
+          <ThumbUpAltIcon fontSize="small"/>&nbsp;Like&nbsp;{post.likeCount}
         </Button>
         <Button 
           size="small" 
           color="primary" 
           onClick={() => dispatch(deletePost(post._id))}>
-          <DeleteIcon fontSize="small"/> Delete
+          <DeleteIcon fontSize="small"/>&nbsp;Delete
         </Button>
       </CardActions>
     </Card>
