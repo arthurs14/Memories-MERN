6c6e65a94c864b0893b64baf012e60bdf5539959
arthurs14 <arthurcs426@gmail.com>
2021-07-07 17:42:04 -0700

    added delete functionality and like functionality of our cards

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,11 +4,14 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
-
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+
+import { deletePost, likePost } from '../../../actions/postActions';
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   return (
     <Card className={classes.card}>
@@ -46,10 +49,16 @@ const Post = ({ post, setCurrentId }) => {
           gutterBottom>{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button 
+          size="small" 
+          color="primary" 
+          onClick={() => dispatch(likePost(post._id))}>
           <ThumbUpAltIcon fontSize="small"/> Like {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button 
+          size="small" 
+          color="primary" 
+          onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize="small"/> Delete
         </Button>
       </CardActions>
--- client/src/reducers/postReducers.js
+++ client/src/reducers/postReducers.js
@@ -2,6 +2,10 @@ const initialState = [];
 
 const reducer = (posts = initialState, action) => {
   switch(action.type) {
+    case 'LIKE':
+      return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+    case 'DELETE':
+      return posts.filter(post => post._id !== action.payload);
     case 'UPDATE':
       return posts.map((post) => post._id === action.payload._id ? action.payload : post);
     case 'FETCH_ALL':
