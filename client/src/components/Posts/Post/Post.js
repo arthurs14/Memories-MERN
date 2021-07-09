416ac2c46003ad614b968f7658c54f33a72873b2
arthurs14 <arthurcs426@gmail.com>
2021-07-08 19:32:06 -0700

    hid edit and delete button when its not the registered user who made it

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -39,14 +39,17 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant="h6">{post.name}</Typography>
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
       </div>
-      <div className={classes.overlay2}>
-        <Button 
-          style={{ color: 'white' }}
-          size="small"
-          onClick={() => setCurrentId(post._id)}>
-            <MoreHorizIcon fontSize="default" />
-        </Button>
-      </div>
+      {
+        user?.result?._id === post?.creator &&
+        <div className={classes.overlay2}>
+          <Button 
+            style={{ color: 'white' }}
+            size="small"
+            onClick={() => setCurrentId(post._id)}>
+              <MoreHorizIcon fontSize="default" />
+          </Button>
+        </div>
+      }
       <div className={classes.details}>
         <Typography 
           variant="body2"
@@ -72,12 +75,15 @@ const Post = ({ post, setCurrentId }) => {
           onClick={() => dispatch(likePost(post._id))}>
           <Likes />
         </Button>
-        <Button 
-          size="small" 
-          color="primary" 
-          onClick={() => dispatch(deletePost(post._id))}>
-          <DeleteIcon fontSize="small"/>&nbsp;Delete
-        </Button>
+        {
+          (user?.result?._id === post?.creator) && 
+          <Button 
+            size="small" 
+            color="primary" 
+            onClick={() => dispatch(deletePost(post._id))}>
+            <DeleteIcon fontSize="small"/>&nbsp;Delete
+          </Button>
+        }
       </CardActions>
     </Card>
   );
