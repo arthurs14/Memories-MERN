42c58b93d2b07095bc98e2ff84c68dae1c909e4d
arthurs14 <arthurcs426@gmail.com>
2021-07-08 18:43:44 -0700

    actions for signin and signup done

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,6 +4,10 @@ import { AUTH } from '../constants/actionTypes';
 export const signin = (formData, history) => async(dispatch) => {
   try {
     // log in the user
+    const { data } = await api.signIn(formData);
+
+    dispatch({ type: AUTH, data });
+
     history.push('/');
   } catch (err) {
     console.log(err);
@@ -13,6 +17,10 @@ export const signin = (formData, history) => async(dispatch) => {
 export const signup = (formData, history) => async(dispatch) => {
   try {
     // sign up the user
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data });
+    
     history.push('/');
   } catch (err) {
     console.log(err);
