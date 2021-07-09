cf4d3aa02e89be2a29db53ef5f8d1eb02c60fa30
arthurs14 <arthurcs426@gmail.com>
2021-07-08 19:18:45 -0700

    should be able to log in and add posts by that user

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
 // url pointing to backend route
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+  }
+
+  return req;
+});
 
 export const fetchPosts = () => API.get('/posts');
 export const createPost = (post) => API.post('/posts', post);
@@ -9,5 +17,5 @@ export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updated
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
-export const signIn = (formData) => API.post('/users/signin', formData);
-export const signUp = (formData) => API.post('users/signup', formData);
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const signUp = (formData) => API.post('user/signup', formData);
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -9,7 +9,6 @@ import{ createPost, updatePost } from '../../actions/postActions';
 
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
-    creator: '',
     title: '',
     message: '',
     tags: '',
@@ -17,8 +16,9 @@ const Form = ({ currentId, setCurrentId }) => {
   });
   const post = useSelector(state => currentId ? state.posts.find(post => post._id === currentId) : null);
   const classes = useStyles();
-
   const dispatch = useDispatch();
+
+  const user = JSON.parse(localStorage.getItem('profile'));
 
   useEffect(() => {
     if (post) setPostData(post);
@@ -29,17 +29,27 @@ const Form = ({ currentId, setCurrentId }) => {
     e.preventDefault();
 
     if (currentId) {
-      dispatch(updatePost(currentId, postData));
+      dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
     } else {
-      dispatch(createPost(postData));
+      dispatch(createPost({ ...postData, name: user?.result?.name }));
     }
      
     clear();
   };
 
+  if (!user?.result?.name) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+          Please Sign In to create your own memories and like other's memores.
+        </Typography>
+      </Paper>
+    );
+  }
+
   const clear = () => {
     setCurrentId(null);
-    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    setPostData({ title: '', message: '', tags: '', selectedFile: '' });
   };
    
   return (
@@ -51,14 +61,6 @@ const Form = ({ currentId, setCurrentId }) => {
         onSubmit={handleSubmit}
       >
         <Typography variant="h6">Save a Memory</Typography>
-        <TextField 
-          name="creator"
-          variant="outlined"
-          label="Creator"
-          fullWidth
-          value={postData.creator}
-          onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
-        />
         <TextField 
           name="title"
           variant="outlined"
