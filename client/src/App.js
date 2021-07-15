d0ceded8a7cd2cf19a39e6a53358548b1d824309
arthurs14 <arthurcs426@gmail.com>
2021-07-14 17:17:10 -0700

    added more routes for individual posts and etc.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 
+import PostDetails from './components/PostDetails/PostDetails';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
@@ -8,10 +9,13 @@ import Auth from './components/Auth/Auth';
 const App = () => {
   return (
     <Router>
-      <Container maxwidth='lg'>
+      <Container maxwidth='xl'>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={() => <Redirect to="/posts" />} />
+          <Route exact path="/posts" component={Home} />
+          <Route exact path="/posts/search" component={Home} />
+          <Route path="/posts/:id" component={PostDetails} />
           <Route exact path="/auth" component={Auth} />
         </Switch>
       </Container>
